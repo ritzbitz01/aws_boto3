25139b35ac152ac07dc3a98bd840b1bdb5be4b77
Ritz <ryan.ritz@pearson.com>
2017-06-26 11:33:42 -0600

    Add printouts

--- actions/lib/action.py
+++ actions/lib/action.py
@@ -138,6 +138,7 @@ class BaseAction(Action):
             for k, v in kwargs.items():
                 if not v:
                     del kwargs[k]
+            print("action.py L141 Getting boto3 session with cls " + cls)
             obj = self.get_boto3_session(cls)
         else:
             del self.credentials['region']
@@ -147,6 +148,7 @@ class BaseAction(Action):
             raise ValueError('Invalid or missing credentials (aws_access_key_id,'
                              'aws_secret_access_key) or region')
 
+        print("Credentials at action.py L151 " + self.credentials['region'])
         resultset = getattr(obj, action)(**kwargs)
         formatted = self.resultsets.formatter(resultset)
         return formatted if isinstance(formatted, list) else [formatted]
